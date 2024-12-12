2398f082cd052212139674f31e3b8cfa4161a162
carttman <93562297+carttman@users.noreply.github.com>
2024-12-12 19:51:22 +0900

    버블 소트

--- Backjoon/1377_버블 소트.cpp
+++ Backjoon/1377_버블 소트.cpp
@@ -19,20 +19,20 @@ int main()
 	{
 		cin >> A[i].first; // 숫자 저장
 		A[i].second = i; // 해당 인덱스 저장
-
-
 	}
 
-	sort(A.begin(), A.end());
+	sort(A.begin(), A.end()); // 오름차순 정렬
 
 	int Max = 0;
 
 	for(int i=0; i< N; i++)
 	{
-		if(Max < A[i].second - i)
+		if(Max < A[i].second - i) //숫자의 정렬 전 index - 정렬 후 index 계산값의 최댓값 찾고 저장한다.
 		{
-			Max = A[i].second-i;
+			Max = A[i].second - i;
 		}
 	}
+
+
 	cout << Max + 1;
 }
