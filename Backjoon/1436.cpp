53f3818ef5e7db117d6056a31f35183d32b15de8
carttman <chj990920@gmail.com>
2025-03-17 00:15:03 +0900

    11650, 1436

---
+++ Backjoon/1436.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+
+	cin >> N;
+
+	//666이 붙어있는 수 중에서
+	//N번째에 있는 가장 작은 수를 출력
+
+	
+}
