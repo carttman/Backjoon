2dfc87f630449c9bd0aac10ca4125b54c887dfb3
carttman <chj990920@gmail.com>
2025-03-17 14:17:14 +0900

    11650, 1436

--- Backjoon/11650.cpp
+++ Backjoon/11650.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -8,5 +9,24 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int N;
 
+	cin >> N;
+
+	vector<pair<int, int>> A(N, make_pair(0, 0));
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i].first >> A[i].second;
+
+	}
+		sort(A.begin(), A.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << A[i].first  << " " << A[i].second << "\n";
+	}
+
+	// x좌표를 정렬한다
+	// x좌표의 크기가 같다면 y좌표 비교한다
 }
--- Backjoon/1436.cpp
+++ Backjoon/1436.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -15,5 +18,33 @@ int main()
 	//666이 붙어있는 수 중에서
 	//N번째에 있는 가장 작은 수를 출력
 
+	//1666, 2666,  ... 5666, 6660
+	int num = 665;
+	int cnt = 0;
+
+	vector<int> ans;
+	while (1)
+	{
+		num++;
+		// nmu을 증가시킨다.
+		// num을 string으로 변환시킨다.
+		string sNum = to_string(num);
+
+		// string.find로 '666' 탐색한다.
+		if (sNum.find("666") != string::npos)
+		{
+		//666 있으면 정수 배열에 num 담는다
+			ans.push_back(num);
+			cnt++;
+		}
+		
+		//
+		if (cnt == N)
+		{
+			cout << ans[cnt-1];  
+			return 0;
+		}
+	}
+	
 	
 }
