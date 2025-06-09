a3049507ba612ab03b274ddea7b1b178f058a655
carttman <chj990920@gmail.com>
2025-06-09 23:35:14 +0900

    2775_부녀회장이 될테야

--- Backjoon/2775_부녀회장이 될테야.cpp
+++ Backjoon/2775_부녀회장이 될테야.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
+int Re(int floor, int ho)
+{
+	int sum = 0;
+	
+	if (floor == 0)
+	{
+		return ho;
+	}
+	else if (ho == 1)
+	{
+		return 1;
+	}
+	else
+	{	// k-1층 n호 + k층 n-1호
+		return Re(floor - 1, ho) + Re(floor, ho-1);
+	}
+}
+
 int main()
 {
-	// 각 층의 사람들을 불러 모은다
+	// a층의 b호에 살려면
+	// a-1층의 1호부터 b호까지 사람들의 수의 합
+	// 주어지는 양의 정수 k와 n에 대해 k층에 n호에는 몇 명 살고있는지
+	// 아파트는 0층부터 있음 / 각 층은 1호부터, 0층의 i호에는 i명이 산다.
+	int t;
 
-	//a-1층의 1호부터 b호까지 사람들의 수
+	cin >> t;
+
+	for (int i=0; i<t; i++)
+	{
+		int k, n;
+		cin >> k >> n;
+
+		cout << Re(k, n) << "\n";
+	}
+
 }
