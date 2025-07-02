12e555e7fac23b032c9ea7e7c8a05b395f3a5f28
carttman <chj990920@gmail.com>
2025-07-02 23:46:07 +0900

    2805_나무 자르기

--- Silver/2805_나무 자르기.cpp
+++ Silver/2805_나무 자르기.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	
+	int n;
+	long long m;
+
+	cin >> n >> m;
+
+	vector<int> trees(n);
+
+	for (int i=0; i<n; i++)
+	{
+		cin >> trees[i];
+	}
+
+	int low = 0;
+	int high = *max_element(trees.begin(), trees.end());
+
+	int result = 0;
+
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+
+		long long total = 0;
+
+		for (int tree : trees)
+		{
+			if (tree > mid)
+			{
+				total += (tree - mid);
+			}
+		}
+
+		if (total >= m)
+		{
+			result = mid;
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+
+	}
+
+	cout << result << "\n";
+
+	return 0;
 }
