053e047f87924d0db66e7fd9d283c02503cedb9d
carttman <chj990920@gmail.com>
2025-07-22 21:15:07 +0900

    1932_정수 삼각형

--- Silver/1932_정수 삼각형.cpp
+++ Silver/1932_정수 삼각형.cpp
@@ -9,7 +9,7 @@ int main()
 	cin >> n;
 
 	vector<vector<int>> triangle(n);
-	int dp[501] = {0,};
+	//int dp[501] = {0,};
 
 	for (int i=0; i<triangle.size(); i++)
 	{
@@ -22,16 +22,46 @@ int main()
 		}
 	}
 
-	dp[0] = triangle[0][0];
-	//dp[1] = max(dp[0] + triangle[1][0], dp[0] + triangle[1][1]);
+	//dp[0] = triangle[0][0];
 	
-	for (int i=1; i<triangle.size(); i++)
+	/*for (int i=1; i<n; i++)
 	{
 		for (int j=1; j<triangle[i].size(); j++)
 		{
 			dp[i] = max(dp[i-1] + triangle[i][j-1], dp[i-1] + triangle[i][j]);
 		}
-	}
+	}*/
 
-	cout << dp[n-1];
+	vector<vector<int>> copy(n);
+	copy = triangle;
+
+	copy[1][0] = copy[1][0] + copy[0][0];
+	copy[1][1] = copy[1][1] + copy[0][0];
+
+
+	for (int i=2; i<n; i++)
+	{
+		for (int j=1; j<=copy[i].size(); j++)
+		{	
+			if (j==1)//맨 왼쪽
+			{
+				copy[i][j-1] = copy[i][j-1] + copy[i-1][j-1];
+				
+			}
+			else if (j == copy[i].size())//맨 오른쪽
+			{
+				copy[i][j - 1] = copy[i][j - 1] + copy[i - 1][j - 1];
+			}
+			else
+			{
+				copy[i][j-1] = copy[i][j-1] + max(copy[i-1][j-1], copy[i-1][j]);
+			}
+
+			
+		}
+	}
+	
+
+	cout << 1;
+	//cout << dp[n-1];
 }
