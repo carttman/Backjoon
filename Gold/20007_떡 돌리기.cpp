699b99033844ec93d0965e6d52fb6a91661940b4
carttman <chj990920@gmail.com>
2025-07-28 18:31:33 +0900

    20007_떡 돌리기

--- Gold/20007_떡 돌리기.cpp
+++ Gold/20007_떡 돌리기.cpp
@@ -1,26 +1,95 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-int MAX = 100001;
+constexpr int max_size = 10000001;
+
+struct cmp
+{
+	bool operator()(pair<int, int>& a, pair<int, int>& b) const
+	{
+		if (a.second == b.second)
+		{
+			return a.first > b.first;
+		}
+
+		return a.second > b.second;
+	}
+};
 
 int main()
 {
-	int n, m, x, y;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	int n, m, x, y;
 	cin >> n >> m >> x >> y;
-	vector<vector<int>> graph(n, vector<int>(n, MAX));
+
+	vector<vector<pair<int,int>>> graph(n);
+	vector<int> dist(n, max_size);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+
+	pq.emplace(y, 0);
+	dist[y] = 0;
+
 
 	for (int i=0; i<m; i++)
 	{
 		int s, e, v;
 		cin >> s >> e >> v;
-
-		graph[s][e] = v;
-		graph[e][s] = v;
+		graph[s].emplace_back(e, v);
+		graph[e].emplace_back(s, v);
 	}
 
-	cout << 1;
+	while (!pq.empty())
+	{
+		pair<int, int> curr_edge = pq.top();
+		int curr_node = curr_edge.first;
+		int curr_weight = curr_edge.second;
 
+		pq.pop();
+
+		for (const pair<int,int>& edge : graph[curr_node])
+		{
+			int next_node = edge.first;
+			int next_weight = edge.second;
+
+			if (dist[next_node] > curr_weight + next_weight)
+			{
+				dist[next_node] = curr_weight + next_weight;
+
+				pq.emplace(next_node, dist[next_node]);
+			}
+		}
+	}
+
+	for (const int& value : dist)
+	{
+		if (x < value)
+		{
+			cout << -1;
+			return 0;
+		}
+	}
+
+	sort(dist.begin(), dist.end());
+
+	int sum = 0;
+	int day = 1;
+	for (const int& n : dist)
+	{
+		sum += n * 2;
+		if (x < sum)
+		{
+			day++;
+			sum = n * 2;
+		}
+	}
+
+	cout << day;
+
+	return 0;
 }
