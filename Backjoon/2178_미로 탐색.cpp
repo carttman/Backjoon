944f99649bcedb5135579d8fc39e920f1c592284
carttman <chj990920@gmail.com>
2025-08-01 18:17:11 +0900

    2178_미로 탐색

--- Backjoon/2178_미로 탐색.cpp
+++ Backjoon/2178_미로 탐색.cpp
@@ -1,77 +1,67 @@
 #include <iostream>
 #include <queue>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-// 오, 아 , 왼 , 위(시계방향)
-int Row[4] = {1, 0, -1, 0}; // x
-int Column[4] = {0, 1, 0, -1}; // y
-
-//맵, 방문 초기화
-int Map[101][101];
-bool Visited[101][101] = {false,}; 
-
-int N, M;
-
-void BFS(int x, int y)
+int main()
 {
-	queue<pair<int, int>> q;
-	q.push(make_pair(x, y)); // 큐에 현재 좌표 삽입
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	int n, m;
+	cin >> n >> m;
 
-	while(!q.empty()) // 큐가 빌때까지 반복
+	vector<vector<int>> mymap(n, vector<int>(m, 0));
+
+	for (int i=0; i<n; i++)
 	{
-		int curr[2]; //현재 좌표를 기억하고 큐에서 지운다
-		curr[0] = q.front().first; // x
-		curr[1] = q.front().second; // y
+		string s;
+		cin >> s;
+
+		for (int j=0; j<s.size(); j++)
+		{
+			int si = s[j] - '0';
+
+			mymap[i][j] = si;
+		}
+	}
+
+	vector<pair<int, int>> dir = {{1,0}, {-1,0}, {0,-1}, {0, 1}};
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+	queue<pair<int, int>> q;
+	q.emplace(0,0);
+
+	while (!q.empty())
+	{
+		auto current = q.front();
+		int c_row = current.first;
+		int c_col = current.second;
+
 		q.pop();
 
-		Visited[x][y] = true; // 현재 좌표 방문했다
-
-		for(int i=0; i<4; i++) // 오 아 왼 위 탐색
+		for (int i=0; i<4; i++)
 		{
-			int row = curr[0] + Row[i]; 
-			int column = curr[1] + Column[i]; 
+			int n_row = c_row + dir[i].first;
+			int n_col = c_col + dir[i].second;
 
-			if(row >= 0 && column >= 0) // 아예 없는 길인지 체크 1
+			if (n_row >= 0 && n_row < n && n_col >= 0 && n_col < m)
 			{
-				if(column < N && row < M)// 아예 없는 길인지 체크 2
+				if (mymap[n_row][n_col] > 0)
 				{
-					if(Map[column][row] != 0) // 갈 수 있는 길인지 체크
+					if (!visited[n_row][n_col])
 					{
-						if(!Visited[column][row]) // 방문 한 곳인지 체크
-						{
-							Visited[column][row] = true; //방문했다
-
-							Map[column][row] = Map[curr[1]][curr[0]] + 1; //
-							q.push(make_pair(row, column));
-						}
+						visited[n_row][n_col] = true;
+						mymap[n_row][n_col] = mymap[c_row][c_col] + 1;
+						q.emplace(n_row, n_col);
 					}
 				}
 			}
 		}
 	}
 
-
-}
-
-int main()
-{
-	cin >> N >> M;
-
-	for(int i=0; i < N; i++) // 4
-	{
-		string k;
-		cin >> k;
-		for(int j=0; j < M; j++) // 6
-		{
-			int a = k[j] - '0';
-			Map[i][j] = a;
-		}
-	}
-
-	BFS(0,0);
-
-	cout << Map[N-1][M-1] << endl;
-	return 0;
+	cout << mymap[n-1][m-1];
 }
