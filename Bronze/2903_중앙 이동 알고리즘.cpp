5e475bd15839efba3cdfd7d09a3b14d46657ba48
carttman <chj990920@gmail.com>
2025-08-05 23:27:10 +0900

    2903_중앙 이동 알고리즘

--- Bronze/2903_중앙 이동 알고리즘.cpp
+++ Bronze/2903_중앙 이동 알고리즘.cpp
@@ -7,4 +7,16 @@ int main()
 	int n;
 	cin >> n;
 
+	int length = 2;
+	int ans;
+
+	for (int i=0; i<n; i++)
+	{
+		ans = (length * 2 - 1) * (length * 2 - 1);
+		length = length * 2-1;
+	}
+
+	cout << ans;
+
+	return 0;
 }
