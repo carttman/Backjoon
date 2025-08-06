b1445e8729f937a96e4e83314fe61feb783caf8f
carttman <chj990920@gmail.com>
2025-08-06 20:24:31 +0900

    2745_진법 변환

--- Bronze/2745_진법 변환.cpp
+++ Bronze/2745_진법 변환.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <map>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	// B진법 N
-	// 10진법
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	map<char, int> mp36 = {
+		{'0', 0},{'1', 1},{'2', 2},{'3', 3},{'4', 4},{'5', 5},{'6', 6},
+		{'7', 7},{'8', 8},{'9', 9},{'A', 10}, {'B', 11}, {'C', 12}, {'D', 13},
+		{'E', 14}, {'F', 15}, {'G', 16},{'H', 17}, {'I', 18}, {'J', 19},{'K', 20},
+		{'L', 21}, {'M', 22}, {'N', 23}, {'O', 24},{'P', 25}, {'Q', 26}, {'R', 27},
+		{'S', 28}, {'T', 29}, {'U', 30}, {'V', 31}, {'W', 32},{'X', 33}, {'Y', 34}, {'Z', 35} };
 
 	string s;
 	cin >> s;
@@ -13,6 +23,14 @@ int main()
 	int b;
 	cin >> b;
 
+	int base10 = 0;
 
+	for (int i = 0; i < s.size(); i++)
+	{
+		base10 += mp36[s[i]] * (pow(b, s.size() - i - 1));
+	}
 
+	cout << base10;
+
+	return 0;
 }
