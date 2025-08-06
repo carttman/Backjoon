bc0ceab3d4439b90813e2d68ebba769d25b6e900
carttman <chj990920@gmail.com>
2025-08-06 20:24:43 +0900

    11005_진법 변환2.

---
+++ Bronze/11005_진법 변환2.cpp
@@ -0,0 +1,35 @@
+#include <deque>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	map<int, string> mp36 = {
+ {0, "0"}, {1, "1"}, {2, "2"}, {3, "3"}, {4, "4"}, {5, "5"}, {6, "6"}, {7, "7"}, {8, "8"}, {9, "9"},
+	{10, "A"}, {11, "B"}, {12, "C"}, {13, "D"},
+	{14, "E"}, {15, "F"}, {16, "G"}, {17, "H"}, {18, "I"}, {19, "J"}, {20, "K"},
+	{21, "L"}, {22, "M"}, {23, "N"}, {24, "O"}, {25, "P"}, {26, "Q"}, {27, "R"},
+	{28, "S"}, {29, "T"}, {30, "U"}, {31, "V"}, {32, "W"}, {33, "X"}, {34, "Y"}, {35, "Z"}
+	};
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, b;
+	cin >> n >>  b;
+
+	string s;
+	
+	while (n > 0)
+	{
+		string m = mp36[n % b];
+		s.insert(0, m);
+		
+		n /= b;
+	}
+	cout << s;
+	return 0;
+}
