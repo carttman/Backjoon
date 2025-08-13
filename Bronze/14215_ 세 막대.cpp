5217442087a482f4b5ca9ef98c17add499769169
carttman <chj990920@gmail.com>
2025-08-13 23:23:57 +0900

    Update 14215_ 세 막대.cpp

--- Bronze/14215_ 세 막대.cpp
+++ Bronze/14215_ 세 막대.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -8,9 +9,15 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int a, b, c;
-	cin >> a >> b >> c;
-	
+	int arr[3];
+	cin >> arr[0] >> arr[1] >> arr[2];
+
+	sort(arr, arr + 3);
+
+	if (arr[0] + arr[1] <= arr[2])
+		arr[2] = arr[0] + arr[1] - 1;
+
+	cout << arr[0] + arr[1] + arr[2];
 
 	return 0;
 }
