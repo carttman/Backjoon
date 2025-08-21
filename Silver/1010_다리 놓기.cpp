35b3b900f7d43a687d1b5c3ac9732168b2351a0b
carttman <chj990920@gmail.com>
2025-08-21 10:20:08 +0900

    Create 1010_다리 놓기.cpp

---
+++ Silver/1010_다리 놓기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+		long answer = 1;
+		int k = 1;
+
+		for (int j=m; j>m-n; j--)
+		{
+			answer *= j;
+			answer /= k;
+			k++;
+		}
+
+		cout << answer << "\n";
+	}
+
+	return 0;
+}
