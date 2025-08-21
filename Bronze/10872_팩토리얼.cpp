03a7241ecede032b86dcf61d89ef73d811771944
carttman <chj990920@gmail.com>
2025-08-21 10:19:59 +0900

    Create 10872_팩토리얼.cpp

---
+++ Bronze/10872_팩토리얼.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+long long recursion(int n)
+{
+	if (!n || n == 1)
+		return 1;
+
+	return n * recursion(n-1);
+}
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << recursion(n);
+	return 0;
+}
