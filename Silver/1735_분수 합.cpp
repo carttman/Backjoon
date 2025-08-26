0de4918bbc4670873150ba4180b9da3a26e14574
carttman <chj990920@gmail.com>
2025-08-27 00:48:45 +0900

    Update 1735_분수 합.cpp

--- Silver/1735_분수 합.cpp
+++ Silver/1735_분수 합.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+int gcd(int x, int y)
+{
+	if (y)
+	{
+		return gcd(y, x % y);
+	}
+
+	return x;
+
+}
+
 int main()
 {
-	
+	int x1, y1, x2, y2;
 
+	cin >> x1 >> y1 >> x2 >> y2;
+
+
+	int child = (x1 * y2) + (x2 * y1);
+	int parent = y1 * y2;
+
+	cout << child / gcd(child, parent) << " " << parent / gcd(child ,parent);
 	return 0;
 }
