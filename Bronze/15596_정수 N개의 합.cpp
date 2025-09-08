498a623590acf096148cc7322cdc10ba385a811f
carttman <chj990920@gmail.com>
2025-09-09 00:33:39 +0900

    Create 15596_정수 N개의 합.cpp

---
+++ Bronze/15596_정수 N개의 합.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long sum(vector<int>& a)
+{
+	long long ans = 0;
+	for (const auto& i : a)
+	{
+		ans += i;
+	}
+
+	return ans;
+}
+
+int main()
+{
+
+	vector<int> a;
+
+	return 0;
+}
