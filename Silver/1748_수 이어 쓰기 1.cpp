4bb486f24e099fc9d7e82f2589c6c0052ee0c12b
carttman <chj990920@gmail.com>
2025-09-16 21:32:46 +0900

    Create 1748_수 이어 쓰기 1.cpp

---
+++ Silver/1748_수 이어 쓰기 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int ans = 0;
+
+	for (int i = 1; i <= n; i *= 10)
+	{
+		ans += n - i + 1;
+	}
+
+	cout << ans;
+	return 0;
+}
