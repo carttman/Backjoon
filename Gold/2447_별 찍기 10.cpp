ad1d9af54c0032e96ac0b8d6da5288cf2b1a80dd
carttman <chj990920@gmail.com>
2025-09-25 00:27:20 +0900

    Update 2447_별 찍기 10.cpp

--- Gold/2447_별 찍기 10.cpp
+++ Gold/2447_별 찍기 10.cpp
@@ -2,27 +2,30 @@
 
 using namespace std;
 
+void star(int i, int j, int num)
+{
+	if ((i / num) % 3 == 1 && (j / num) % 3 == 1)
+		cout << ' ';
+	else
+	{
+		if (num / 3 == 0)
+			cout << '*';
+		else
+			star(i, j, num / 3);
+	}
+}
+
 int main()
 {
 	int n;
 	cin >> n;
 
-	for (int i = 0; i < 3 * 3; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < 3; j++)
-		{
-			cout << "*";
-		}
-
-		for (int j=0; j<3; j++)
-		{
-			
-		}
-
-		for (int j = 0; j < 3; j++)
-		{
-			cout << "*";
-		}
+		for (int j = 0; j < n; j++)
+			star(i, j, n);
+		
+		cout << '\n';
 	}
 
 	return 0;
