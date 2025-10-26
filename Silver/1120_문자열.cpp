d6a908c30f44a0ba129d81968b33f578505ec9bf
carttman <chj990920@gmail.com>
2025-10-26 12:40:58 +0900

    Create 1120_문자열.cpp

---
+++ Silver/1120_문자열.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+
+	int ans = 210000000;
+
+	for (int i=0; i<= b.size() - a.size(); i++)
+	{
+		int cnt = 0;
+
+		for (int j=0; j< a.size(); j++)
+			if (a[j] != b[j+i])
+				cnt++;
+
+		ans = min(ans, cnt);
+	}
+
+	cout << ans;
+
+}
