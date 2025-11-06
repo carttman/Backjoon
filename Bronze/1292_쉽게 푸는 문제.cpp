c8709b35dfc697bce6ed5ed5f7764d2b8351b11e
carttman <chj990920@gmail.com>
2025-11-06 12:50:28 +0900

    Update 1292_쉽게 푸는 문제.cpp

--- Bronze/1292_쉽게 푸는 문제.cpp
+++ Bronze/1292_쉽게 푸는 문제.cpp
@@ -1,11 +1,30 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	int a, b;
+	cin >> a >> b;
 
+	vector<int> nums;
+
+	for (int i = 1; i <= 1000; i++)
+	{
+		for (int j = 0; j < i; j++)
+			nums.push_back(i);
+	}
+
+	int sum = 0;
+	for (int i = a; i <= b; i++)
+		sum += nums[i-1];
+
+	cout << sum;
 	return 0;
 }
