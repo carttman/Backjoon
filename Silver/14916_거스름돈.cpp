99c3a8872fb8b1860eb2675fd865c2426c31e4e2
carttman <chj990920@gmail.com>
2025-11-10 11:12:10 +0900

    Create 14916_거스름돈.cpp

---
+++ Silver/14916_거스름돈.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, count = 0;
+	cin >> n;
+
+	if (n % 5 == 0)
+		cout << n / 5;
+	else
+	{
+		while (n > 0)
+		{
+			n -= 2;
+			count++;
+
+			if (n % 5 == 0)
+			{
+				cout << count + n / 5;
+				break;
+			}
+		}
+	}
+
+	if (n < 0)
+		cout << -1;
+
+	return 0;
+}
