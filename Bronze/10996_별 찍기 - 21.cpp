86f9e487241a96d38658c22cbab0d1c15067f31f
carttman <chj990920@gmail.com>
2025-11-24 13:46:39 +0900

    Create 10996_별 찍기 - 21.cpp

---
+++ Bronze/10996_별 찍기 - 21.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n * 2; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i % 2 == 0)
+			{
+				if (j % 2 == 0)
+					cout << '*';
+				else
+					cout << ' ';
+			}
+			else
+			{
+				if (j % 2 == 0)
+					cout << ' ';
+				else
+					cout << '*';
+			}
+		}
+
+		if (n == 1) {}
+		else if (i < n * 2 - 1)
+			cout << '\n';
+
+	}
+
+	return 0;
+}
