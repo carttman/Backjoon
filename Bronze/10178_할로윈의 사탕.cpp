92e9b4398b535a2e923f36c8f7c0341b3d0f724e
carttman <chj990920@gmail.com>
2025-11-28 21:08:35 +0900

    Create 10178_할로윈의 사탕.cpp

---
+++ Bronze/10178_할로윈의 사탕.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	while (n--)
+	{
+		int c, v;
+		cin >> c >> v;
+
+		cout << "You get " << c / v << " piece(s) and your dad gets " << c % v << " piece(s).\n";
+	}
+
+	return 0;
+}
