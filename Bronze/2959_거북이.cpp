1001b28db0afe18bb8c917c0bdd2d8d3b80adfd1
carttman <chj990920@gmail.com>
2025-11-29 18:11:27 +0900

    Create 2959_거북이.cpp

---
+++ Bronze/2959_거북이.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int arr[4];
+	for (int i=0; i<4; i++)
+		cin >> arr[i];
+
+	sort(arr, arr+4);
+
+	cout << arr[0] * arr[2];
+	return 0;
+}
