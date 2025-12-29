f79e9e3f8a5bdb021acd2b9b386bcaf536c7f48c
carttman <chj990920@gmail.com>
2025-12-29 21:37:14 +0900

    Create 13311_행운의 편지.cpp

---
+++ Bronze/13311_행운의 편지.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << -1;
+
+	return 0;
+}
