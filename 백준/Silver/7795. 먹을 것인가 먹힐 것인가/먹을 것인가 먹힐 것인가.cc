d61c19afdf756dece8758e2f9f4740378b7726b1
carttman <93562297+carttman@users.noreply.github.com>
2026-01-25 20:24:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 964 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+		vector<int> a(n);
+		for (int i=0; i<n; i++)
+			cin >> a[i];
+
+		vector<int> b(m);
+		for (int i=0; i<m; i++)
+			cin >> b[i];
+
+		sort(a.begin(), a.end());
+		sort(b.begin(), b.end());
+
+		int cnt = 0;
+		for (int i=0; i<n; i++)
+		{
+			for (int j=0; j<m; j++)
+			{
+				if (a[i] > b[j])
+					cnt++;
+			}
+		}
+
+		cout << cnt << '\n';
+	}
+
+	return 0;
+}
