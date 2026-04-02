9be46f592d499c1eb524adeecc1b3b542d795e1f
carttman <93562297+carttman@users.noreply.github.com>
2026-04-02 16:00:58 +0900

    [Silver III] Title: 두 수의 합, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,13 +1,10 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
 	int n;
 	cin >> n;
 
@@ -21,9 +18,8 @@ int main()
 	int x;
 	cin >> x;
 
-	int r = n - 1;
-	int l = 0;
 	int cnt = 0;
+	int l(0), r(n - 1);
 	while (l < r)
 	{
 		if (a[l] + a[r] == x)
@@ -38,6 +34,5 @@ int main()
 	}
 
 	cout << cnt;
-
 	return 0;
 }
