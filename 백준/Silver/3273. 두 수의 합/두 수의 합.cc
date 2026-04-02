4d35060e094ac14f403da27ce7a2e33a567d04cf
carttman <93562297+carttman@users.noreply.github.com>
2026-04-02 16:09:19 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -3,18 +3,73 @@
 #include <algorithm>
 using namespace std;
 
+int n;
+vector<int> arr(n);
+vector<int> temp(n);
+
+void merge(int left, int mid, int right)
+{
+	int l, m, t;
+
+	l = left;
+	m = mid + 1;
+	t = left;
+
+	while (l <= mid && m <= right)
+	{
+		if (arr[l] <= arr[m])
+			temp[t++] = arr[l++];
+		else
+			temp[t++] = arr[m++];
+	}
+
+	if (l > mid)
+	{
+		while (m <= right)
+			temp[t++] = arr[m++];
+	}
+	else
+	{
+		while (l <= mid)
+			temp[t++] = arr[l++];
+	}
+
+	while (left <= right)
+	{
+		arr[left] = temp[left];
+		left++;
+	}
+}
+
+void merge_sort(int left, int right)
+{
+	int mid;
+
+	if (left >= right)
+		return;
+
+	mid = (left + right) / 2;
+	merge_sort(left, mid);
+	merge_sort(mid + 1, right);
+	merge(left, mid, right);
+}
+
 int main()
 {
-	int n;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	cin >> n;
 
-	vector<int> a(n);
+	arr.resize(n, 0);
+	temp.resize(n, 0);
 
 	for (int i = 0; i < n; i++)
-		cin >> a[i];
+		cin >> arr[i];
 
-	sort(a.begin(), a.end());
-
+	//sort(arr.begin(), arr.end());
+	merge_sort(0, n - 1);
 	int x;
 	cin >> x;
 
@@ -22,12 +77,12 @@ int main()
 	int l(0), r(n - 1);
 	while (l < r)
 	{
-		if (a[l] + a[r] == x)
+		if (arr[l] + arr[r] == x)
 		{
 			cnt++;
 			r--;
 		}
-		else if (a[l] + a[r] < x)
+		else if (arr[l] + arr[r] < x)
 			l++;
 		else
 			r--;
